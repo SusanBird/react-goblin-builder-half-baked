b4b2be894fc058f33ffbdf098a40537e2be64434
SusanBird <susanbird391@gmail.com>
2022-05-04 17:14:23 -0700

    created GoblinForm

--- src/GoblinForm.js
+++ src/GoblinForm.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function GoblinForm(props) {
+export default function GoblinForm({ submitGoblin, setGoblinFormName, setGoblinFormHP, setGoblinFormColor }) {
   return (
     <div className='goblin-form-container quarter'>
+      <form onSubmit={submitGoblin} />
       {/* on submit, call the submitGoblin function, passed in as a prop.
       Note that you don't need to define an anonymous function--you can just name the submitGoblin prop here and it will work.
       Take a minute to try and puzzle out why that is: how is this function different from other functions, 
@@ -11,19 +12,19 @@ export default function GoblinForm(props) {
         <label>
             Name
           {/* onChange, use the prop setGoblinFormName to set the parent state */}
-          <input required value={props.goblinFormName} />
+          <input required value={submitGoblin.goblinFormName} onChange={e => setGoblinFormName(e.target.value)} />
           {/* note that we're controlling the input's value from parent state */}
         </label>
         <label>
             HP
           {/* onChange, use the prop setGoblinFormHP to set the parent state */}
-          <input required type="number" value={props.goblinFormHP} />
+          <input required type="number" value={submitGoblin.goblinFormHP} onChange={e => setGoblinFormHP(e.target.value)} />
           {/* note that we're controlling the input's value from parent state */}
         </label>
         <label>
             Color
           {/* onChange, use the prop setGoblinFormColor to set the parent state */}
-          <select required value={props.goblinFormColor}>
+          <select required value={submitGoblin.goblinFormColor} onChange={e => setGoblinFormColor(e.target.value)}>
             {/* note that we're controlling the input's value from parent state */}
             <option value="lightgreen">Green</option>
             <option value="lightblue">Blue</option>
